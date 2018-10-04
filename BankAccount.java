3c7297daf46117c886b877551a6110b6ad5c58c1
Elizabeth Sham <esham00@stuy.edu>
2018-10-03 23:05:04 -0400

    Withdraw and Deposit functions work

--- BankAccount.java
+++ BankAccount.java
@@ -24,6 +24,7 @@ public class BankAccount {
 	}
 	public boolean deposit(double amount) {
 		if (amount >= 0) {
+			balance += amount;
 			return true;
 		}
 		else {
@@ -32,6 +33,7 @@ public class BankAccount {
 	}
 	public boolean withdraw(double amount) {
 		if (amount <= balance) {
+			balance -= amount;
 			return true;
 		}
 		else {
--- Driver.java
+++ Driver.java
@@ -5,6 +5,29 @@ public class Driver {
      cash.setPassword("newpassword123");
      //toString test + changing password test
      System.out.print("\n" + cash + "\t" + cash.getPassword());
-
+     if (cash.withdraw(30000)) {
+       System.out.print ("\nWithdrawal succeeded" + ", Balance is now " + cash.getBalance());
+     }
+     else {
+       System.out.print ("\nWithdrawal failed");
+     }
+     if (cash.withdraw(10)) {
+       System.out.print ("\nWithdrawal succeeded" + ", Balance is now " + cash.getBalance());
+     }
+     else {
+       System.out.print ("\nWithdrawal failed");
+     }
+     if (cash.deposit(20000)) {
+       System.out.print("\nDeposit succeeded, Balance is now " + cash.getBalance());
+     }
+     else {
+       System.out.print("\nDeposit failed");
+     }
+     if (cash.deposit(-10)) {
+       System.out.print("\nDeposit succeeded, Balance is now " + cash.getBalance());
+     }
+     else {
+       System.out.print("\nDeposit failed");
+     }
   }
 }
