8e38d89cd7dc246ea171718ea256e84552b745d9
Elizabeth Sham <esham00@stuy.edu>
2018-10-03 22:56:31 -0400

    toString function and changing password function works

--- Driver.java
+++ Driver.java
@@ -2,5 +2,9 @@ public class Driver {
    public static void main(String[] args) {
      BankAccount cash = new BankAccount( 4980, 1000.10, "password123" );
      System.out.print( "Balance: " + cash.getBalance() + "\nAccount ID: " + cash.getAccountID() + "\nPassword: " + cash.getPassword());
+     cash.setPassword("newpassword123");
+     //toString test + changing password test
+     System.out.print("\n" + cash + "\t" + cash.getPassword());
+
   }
 }
