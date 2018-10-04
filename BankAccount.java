e5b19009119ea6306f84270f8517f146d8c7f1b2
Elizabeth Sham <esham00@stuy.edu>
2018-10-03 22:35:27 -0400

    wrote functions for bankaccount.java, did not test them yet

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,38 @@
-public class BankAccount { 
+public class BankAccount {
 	private double balance;
 	private int accountID;
 	private String password;
-	public 
+	public BankAccount(int ID, double money, String pass) {
+		accountID = ID;
+		balance = money;
+		password = pass;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public int accountID() {
+		return accountID;
+	}
+	public void setPassword(String newPass) {
+		password = newPass;
+	}
+	public String toString() {
+		return accountID + "\t" + balance;
+	}
+	public boolean deposit(double amount) {
+		if (amount >= 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean withdraw(double amount) {
+		if (amount <= balance) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
